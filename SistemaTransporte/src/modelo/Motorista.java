94a718b54a925fa80304079db4cd7a5b4a4028d6
FernandoSalesi <fernando.k.salesi@unesp.br>
2024-11-03 22:52:36 -0300

    Colocando UML e alterando Motorista

--- SistemaTransporte/src/modelo/Motorista.java
+++ SistemaTransporte/src/modelo/Motorista.java
@@ -1,12 +1,14 @@
 package modelo;
 
 public class Motorista extends UsuarioBase{
- 
+
+    //atributos
     private String cnh; 
     private int experiencia; 
     private String empresa;  
     private String linhaOnibus;
 
+    //construtor
     public Motorista(String nome, int idade, String cpf, String UUID, String senha, String cnh, int experiencia, String empresa, String linhaOnibus) {
         super(nome, idade, cpf, UUID, senha);
         this.cnh = cnh;
@@ -47,6 +49,7 @@ public class Motorista extends UsuarioBase{
         this.linhaOnibus = linhaOnibus;
     }
 
+    //exibir informacoes
     @Override
     public void exibirInfo() {
         super.exibirInfo();
