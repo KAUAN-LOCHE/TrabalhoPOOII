7d538fcc9d5b7690e0f5b885c226f8d60e44c84a
Kauan Loche <127434205+KAUAN-LOCHE@users.noreply.github.com>
2024-11-07 22:44:46 -0300

    Início da implementação da classe Principal

---
+++ SistemaTransporte/src/principal/Principal.java
@@ -0,0 +1,20 @@
+package principal;
+
+import modelo.*;
+import controlador.*;
+import java.util.Scanner;
+
+public class Principal {
+    private int opc = 0;
+    private static SistemaTransporte sistema = new SistemaTransporte();
+    Scanner scan = new Scanner(System.in);
+    
+
+    
+    
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
